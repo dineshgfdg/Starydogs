f2f261b791dbb4bc187331893877e88f2a780ca7
Dinesh <dineshcandy139@gmail.com>
2025-01-08 13:08:30 +0530

    map fix

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com"
+    "default-src 'self'; " +
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+    "font-src 'self' https://fonts.gstatic.com; " +
+    "img-src 'self' data: https: blob:; " +
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com; " +
+    "connect-src 'self' https://*.googleapis.com https://*.gstatic.com https://dog-stray-backend-x7ik.onrender.com;"
   );
   next();
 }); 
